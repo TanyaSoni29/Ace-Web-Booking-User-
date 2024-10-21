f607853b279d8838b698ad9dcb5912fdef8c1177
Abhishek Singh <abhisheksingh4115@gmail.com>
2024-10-21 08:32:44 +0100

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-/** @format */
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LocationForm from './components/LocationForm';
+import VehicleSelection from './components/VehicleSelection';
+import BookingDetails from './components/BookingDetails';
+import Confirmation from './components/Confirmation';
 
 function App() {
-	return (
-		<>
-			<h1>Hello</h1>
-		</>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LocationForm />} />
+                <Route path="/select-vehicle" element={<VehicleSelection />} />
+                <Route path="/booking-details" element={<BookingDetails />} />
+                <Route path="/confirmation" element={<Confirmation />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
